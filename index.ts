5a8c8bced29da4bac8a21b064b5c1beef356028a
GROSST <open.mind2876@gmail.com>
2024-12-18 01:23:08 +0200

    Add rickroll

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import express from "express"
 import https from "https"
-import path  from 'path'
+import path from 'path'
 
 const app = express()
 const port = 3000
 app.use(express.static(__dirname));
 
-app.get("/*", (req, res) => {
+const routes = ["admin", "root", "auth", "token", "ws", "console", "settings", "dashboard", "backend", "secure", "profile"]
 
-    res.status(301).redirect("https://grosst.cc")
+for (let link of routes) {
+    app.get(`/${link}`, (req, res) => {
 
-})
+        res.status(307).redirect("https://youtu.be/dQw4w9WgXcQ")
 
+    })
+}
 
 const server = https.createServer(app);
 server.listen(port, () => {
