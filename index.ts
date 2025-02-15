5693ba5ef9750af5f76d43caa6ecf6004d5f1539
GROSST <open.mind2876@gmail.com>
2025-02-15 17:20:53 +0200

    feat: Implement rickroll functionality with Discord webhook notification

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import express from "express"
+import express, { Request } from "express"
 import https from "https"
 import path from 'path'
+import axios from "axios"
 
 const app = express()
 const port = 3000
@@ -8,9 +9,38 @@ app.use(express.static(__dirname));
 
 const routes = ["admin", "root", "auth", "token", "ws", "console", "settings", "dashboard", "backend", "secure", "profile", "api"]
 
+const rickRolled = async (req: Request) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const path = req.path
+
+    const embed = {
+        "embeds": [
+            {
+                "color": null,
+                "title": "Rick Rolled",
+                "fields": [
+                    {
+                        "name": "path",
+                        "value": `[${path}](https://grosst.cc${path})`,
+                        "inline": true
+                    },
+                    {
+                        "name": "ip",
+                        "value": ip,
+                        "inline": true
+                    }
+                ]
+            }
+        ],
+        "username": "Rick Rolled",
+        "avatar_url": "https://i.postimg.cc/fyBDb1wg/image.png"
+    }
+    await axios.post("https://discord.com/api/webhooks/1204441078623899729/XC_aAK6AKvOKo6yMR7tdslU7M-OHiVOUV9z-DkkX2t3OkhnXbdEgPa3QmG1aOffnousy", embed);
+}
+
 for (let link of routes) {
     app.get(`/${link}`, (req, res) => {
-
+        rickRolled(req)
         res.status(307).redirect("https://youtu.be/dQw4w9WgXcQ")
 
     })
